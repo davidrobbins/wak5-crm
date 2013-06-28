bfff13186f9f0392f6fb02070d8327cf83aa8094
David Robbins <dave@wakanda.org>
2013-06-28 11:43:40 -0700

    Added Dashboard component. Also added CSS rules for Metro Radio Button Tab.

---
+++ wak5-crm/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,24 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var login2 = {};	// @login
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	login2.logout = function login2_logout (event)// @startlock
+	{// @endlock
+		$$('mainComponent').loadComponent({path: '/components/splashScreen.waComponent'});
+	};// @lock
+
+	login2.login = function login2_login (event)// @startlock
+	{// @endlock
+		$$('mainComponent').loadComponent({path: '/components/dashboard.waComponent'});
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("login2", "logout", login2.logout, "WAF");
+	WAF.addListener("login2", "login", login2.login, "WAF");
+// @endregion
+};// @endlock

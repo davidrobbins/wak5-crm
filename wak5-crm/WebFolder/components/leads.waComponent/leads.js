a6a57bbfb7bcf97ac4bba35917b404b9ae79809b
David Robbins <dave@wakanda.org>
2013-07-02 15:42:44 -0700

    Implemented drill-down for Leads.

--- wak5-crm/WebFolder/components/leads.waComponent/leads.js
+++ wak5-crm/WebFolder/components/leads.waComponent/leads.js
@@ -4,7 +4,8 @@
 // Add the code that needs to be shared between components here
 
 function constructor (id) {
-
+	var tabView1 = getHtmlId('tabView1');
+	
 	// @region beginComponentDeclaration// @startlock
 	var $comp = this;
 	this.name = 'leads';
@@ -13,18 +14,32 @@ function constructor (id) {
 	this.load = function (data) {// @lock
 
 	// @region namespaceDeclaration// @startlock
-	var button1 = {};	// @button
+	var dataGrid1 = {};	// @dataGrid
+	var button3 = {};	// @button
+	var button2 = {};	// @button
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
-	button1.click = function button1_click (event)// @startlock
+	dataGrid1.onRowDblClick = function dataGrid1_onRowDblClick (event)// @startlock
 	{// @endlock
-		alert("Under Construction.");
+		$$(tabView1).selectTab(2)
+	};// @lock
+
+	button3.click = function button3_click (event)// @startlock
+	{// @endlock
+		$$(tabView1).selectTab(1)
+	};// @lock
+
+	button2.click = function button2_click (event)// @startlock
+	{// @endlock
+		$$(tabView1).selectTab(1)
 	};// @lock
 
 	// @region eventManager// @startlock
-	WAF.addListener(this.id + "_button1", "click", button1.click, "WAF");
+	WAF.addListener(this.id + "_dataGrid1", "onRowDblClick", dataGrid1.onRowDblClick, "WAF");
+	WAF.addListener(this.id + "_button3", "click", button3.click, "WAF");
+	WAF.addListener(this.id + "_button2", "click", button2.click, "WAF");
 	// @endregion// @endlock
 
 	};// @lock

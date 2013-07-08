7f3605e4a3188529a76c44c2ed2bed2703b8c40b
David Robbins <dave@wakanda.org>
2013-07-08 16:18:22 -0700

    adding buttons for New Lead, Cancel, Save.

--- wak5-crm/WebFolder/components/leads.waComponent/leads.js
+++ wak5-crm/WebFolder/components/leads.waComponent/leads.js
@@ -14,11 +14,38 @@ function constructor (id) {
 	this.load = function (data) {// @lock
 
 	// @region namespaceDeclaration// @startlock
+	var leadNewButton = {};	// @button
+	var leadSaveButton = {};	// @button
+	var leadCancelButton = {};	// @button
 	var button2 = {};	// @button
 	var dataGrid2 = {};	// @dataGrid
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
+
+	leadNewButton.click = function leadNewButton_click (event)// @startlock
+	{// @endlock
+		waf.sources.lead.addNewElement();
+		waf.sources.lead.serverRefresh({
+			onSuccess: function(event) {
+				$$(tabView2).selectTab(2);
+				//$$(firstNameInputfield).focus();
+				//crmUtil.setDisableLeadsQuickAdd("disable");
+				//$$(leadsTitle).setValue("Lead Information");
+			}
+		});
+	};// @lock
+
+	leadSaveButton.click = function leadSaveButton_click (event)// @startlock
+	{// @endlock
+		waf.sources.lead.save();
+		$$(tabView2).selectTab(1);
+	};// @lock
+
+	leadCancelButton.click = function leadCancelButton_click (event)// @startlock
+	{// @endlock
+		$$(tabView2).selectTab(1);
+	};// @lock
 
 	button2.click = function button2_click (event)// @startlock
 	{// @endlock
@@ -31,6 +58,9 @@ function constructor (id) {
 	};// @lock
 
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_leadNewButton", "click", leadNewButton.click, "WAF");
+	WAF.addListener(this.id + "_leadSaveButton", "click", leadSaveButton.click, "WAF");
+	WAF.addListener(this.id + "_leadCancelButton", "click", leadCancelButton.click, "WAF");
 	WAF.addListener(this.id + "_button2", "click", button2.click, "WAF");
 	WAF.addListener(this.id + "_dataGrid2", "onRowDblClick", dataGrid2.onRowDblClick, "WAF");
 	// @endregion// @endlock

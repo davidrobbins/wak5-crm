b6bb205374cbadda591a6ca6cf189ccd45bb2080
David Robbins <dave@wakanda.org>
2013-08-13 19:29:01 -0700

    Added jqPlot charting - jQuery Plotting Plugin.

--- wak5-crm/WebFolder/components/leadStatus.waComponent/leadStatus.js
+++ wak5-crm/WebFolder/components/leadStatus.waComponent/leadStatus.js
@@ -1,17 +1,37 @@
 ﻿
 (function Component (id) {// @lock
-
+	
 // Add the code that needs to be shared between components here
 
 function constructor (id) {
-
+	var chartContainer = getHtmlId('chartContainer');
+	
 	// @region beginComponentDeclaration// @startlock
 	var $comp = this;
 	this.name = 'leadStatus';
 	// @endregion// @endlock
 
 	this.load = function (data) {// @lock
-
+		//console.log($.jqplot);
+		 var data = [
+		    ['Heavy Industry', 12],['Retail', 9], ['Light Industry', 14], 
+		    ['Out of home', 16],['Commuting', 7], ['Orientation', 9]
+		  ];
+		  var plot1 = jQuery.jqplot (chartContainer, [data], 
+		    { 
+		      seriesDefaults: {
+		        // Make this a pie chart.
+		        renderer: jQuery.jqplot.PieRenderer, 
+		        rendererOptions: {
+		          // Put data labels on the pie slices.
+		          // By default, labels show the percentage of the slice.
+		          showDataLabels: true
+		        }
+		      }, 
+		      legend: { show:true, location: 'e' }
+		    }
+		  );
+		
 	// @region namespaceDeclaration// @startlock
 	// @endregion// @endlock
 

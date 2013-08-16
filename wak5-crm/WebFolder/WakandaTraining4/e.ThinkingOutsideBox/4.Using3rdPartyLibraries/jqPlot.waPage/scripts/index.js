1f2d629d2ba10d8b74f2295b072fa25caf3b1c7e
David Robbins <dave@wakanda.org>
2013-08-16 09:39:04 -0700

    call collectionRefresh() when user selects main menu option.

--- wak5-crm/WebFolder/WakandaTraining4/e.ThinkingOutsideBox/4.Using3rdPartyLibraries/jqPlot.waPage/scripts/index.js
+++ wak5-crm/WebFolder/WakandaTraining4/e.ThinkingOutsideBox/4.Using3rdPartyLibraries/jqPlot.waPage/scripts/index.js
@@ -2,57 +2,10 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
-	var plotButton = {};	// @button
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
-	plotButton.click = function plotButton_click (event)// @startlock
-	{// @endlock
-		
-		waf.sources.lead.jqPlotLeadSource({
-			onSuccess: function(ev) {
-				var plot1 = jQuery.jqplot ('chartContainer', [ev.result], 
-			    { 
-			      seriesDefaults: {
-			        // Make this a pie chart.
-			        renderer: jQuery.jqplot.PieRenderer, 
-			        rendererOptions: {
-			          // Put data labels on the pie slices.
-			          // By default, labels show the percentage of the slice.
-			          showDataLabels: true
-			        }
-			      }, 
-			      legend: { show:true, location: 'e' }
-			    }
-			  );
-			}
-		});
-		
-		/*
-		var data = [
-		    ['Heavy Industry', 120],['Retail', 9], ['Light Industry', 14], 
-		    ['Out of home', 16],['Commuting', 7], ['Orientation', 9]
-		  ];
-		  
-		  var plot1 = jQuery.jqplot ('chartContainer', [data], 
-		    { 
-		      seriesDefaults: {
-		        // Make this a pie chart.
-		        renderer: jQuery.jqplot.PieRenderer, 
-		        rendererOptions: {
-		          // Put data labels on the pie slices.
-		          // By default, labels show the percentage of the slice.
-		          showDataLabels: true
-		        }
-		      }, 
-		      legend: { show:true, location: 'e' }
-		    }
-		  );
-		  */
-	};// @lock
-
 // @region eventManager// @startlock
-	WAF.addListener("plotButton", "click", plotButton.click, "WAF");
 // @endregion
 };// @endlock

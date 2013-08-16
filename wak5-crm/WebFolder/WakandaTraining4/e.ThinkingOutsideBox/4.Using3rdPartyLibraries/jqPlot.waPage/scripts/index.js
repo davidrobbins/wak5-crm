8ac78ff2236dcb0f5c07fa4d7a1a2dc2e72da6a1
David Robbins <dave@wakanda.org>
2013-08-16 08:16:20 -0700

    Wakanda Training 4 example on 3rd Party javascript libraries.

--- wak5-crm/WebFolder/WakandaTraining4/e.ThinkingOutsideBox/4.Using3rdPartyLibraries/jqPlot.waPage/scripts/index.js
+++ wak5-crm/WebFolder/WakandaTraining4/e.ThinkingOutsideBox/4.Using3rdPartyLibraries/jqPlot.waPage/scripts/index.js
@@ -2,18 +2,36 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
-	var button1 = {};	// @button
-	var documentEvent = {};	// @document
+	var plotButton = {};	// @button
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
-	button1.click = function button1_click (event)// @startlock
+	plotButton.click = function plotButton_click (event)// @startlock
 	{// @endlock
+		
+		waf.sources.lead.jqPlotLeadSource({
+			onSuccess: function(ev) {
+				var plot1 = jQuery.jqplot ('chartContainer', [ev.result], 
+			    { 
+			      seriesDefaults: {
+			        // Make this a pie chart.
+			        renderer: jQuery.jqplot.PieRenderer, 
+			        rendererOptions: {
+			          // Put data labels on the pie slices.
+			          // By default, labels show the percentage of the slice.
+			          showDataLabels: true
+			        }
+			      }, 
+			      legend: { show:true, location: 'e' }
+			    }
+			  );
+			}
+		});
 		
 		/*
 		var data = [
-		    ['Heavy Industry', 12],['Retail', 9], ['Light Industry', 14], 
+		    ['Heavy Industry', 120],['Retail', 9], ['Light Industry', 14], 
 		    ['Out of home', 16],['Commuting', 7], ['Orientation', 9]
 		  ];
 		  
@@ -31,42 +49,10 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 		      legend: { show:true, location: 'e' }
 		    }
 		  );
-		 */
-		 
-		 
-		 
-		/**/
-		waf.sources.lead.jqPlotLeadSource({
-			onSuccess: function(ev) {
-				var plot1 = jQuery.jqplot ('chartContainer', ev.result, 
-			    { 
-			      seriesDefaults: {
-			        // Make this a pie chart.
-			        renderer: jQuery.jqplot.PieRenderer, 
-			        rendererOptions: {
-			          // Put data labels on the pie slices.
-			          // By default, labels show the percentage of the slice.
-			          showDataLabels: true
-			        }
-			      }, 
-			      legend: { show:true, location: 'e' }
-			    }
-			  );
-			}
-		});
-		
-		
-		
-	};// @lock
-
-	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
-	{// @endlock
-		
-		 
+		  */
 	};// @lock
 
 // @region eventManager// @startlock
-	WAF.addListener("button1", "click", button1.click, "WAF");
-	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+	WAF.addListener("plotButton", "click", plotButton.click, "WAF");
 // @endregion
 };// @endlock

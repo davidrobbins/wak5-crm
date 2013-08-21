1c7080867b2a3c9e08080a0a8385f6f8af9328e1
David Robbins <dave@wakanda.org>
2013-08-21 08:50:56 -0700

    Experimented with moving metro css rules out of application.css. No go. Studio doesn't look at my custom style sheet, only runtime so I moved it back.

--- wak5-crm/Model.js
+++ wak5-crm/Model.js
@@ -12,3 +12,31 @@ include("classes/recentItem.js");
 include("classes/note.js");
 include("classes/log.js");
 
+
+
+/*
+var dataClassNames = []; 
+for (var theName in ds.dataClasses) { // put each datastore class of ds
+ 	dataClassNames.push (theName); // in the array
+}
+
+//model.Lead.collectionMethods.jqPlotLeadStatus = function() {
+	
+	
+for (var i = 0, len = dataClassNames.length; i < len; i++) 
+{
+	model[dataClassNames[i]].collectionMethods.collectionEcho = function() {
+		return "There are " + this.length + " entities in the " + dataClassNames[i] + " collection."
+	};
+}
+*/
+
+/*
+var dataClassNames = [];
+	
+for (var property in model) {
+    dataClassNames.push(property);
+}
+
+dataClassNames
+*/

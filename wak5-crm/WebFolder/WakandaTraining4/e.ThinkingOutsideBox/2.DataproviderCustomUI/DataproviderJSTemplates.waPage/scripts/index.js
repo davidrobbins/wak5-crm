d0956a1b27739ea387a38353593e0d77f1660a78
David Robbins <dave@wakanda.org>
2013-08-23 14:29:47 -0700

    Dataprovider - Javascript Templates example - can now list Leads.

---
+++ wak5-crm/WebFolder/WakandaTraining4/e.ThinkingOutsideBox/2.DataproviderCustomUI/DataproviderJSTemplates.waPage/scripts/index.js
@@ -0,0 +1,50 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+	var itemsUL$ = $('#itemsUL'),
+	//Get jQuery reference to our <ul> for listing the collection.
+	listTemplateSource = $("#list-template").html(),
+	listTemplateFn = Handlebars.compile(listTemplateSource),
+	itemData = "";
+	
+// eventHandlers// @lock
+	function buildItemsList() {
+		itemsUL$.children().remove(); 
+
+		ds.Lead.all({
+			onSuccess: function(ev1) {
+				ev1.entityCollection.forEach({
+					onSuccess: function(ev2) {	
+						itemData = 	{
+							name:  		ev2.entity.fullName.getValue(),
+							city:    	ev2.entity.city.getValue(),
+							dataId: 	ev2.entity.ID.getValue()
+						};
+						itemsUL$.append(listTemplateFn(itemData));
+					}
+				}); //ev1.entityCollection.forEach
+			}
+		});
+	}
+	
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		//event handlers
+		itemsUL$.on('mouseenter', '.itemPreview', function (event) {
+	   		$(this).addClass('itemSelected');
+		});
+
+		itemsUL$.on('mouseleave', '.itemPreview', function (event) {
+	   		$(this).removeClass('itemSelected');
+		});
+		
+		buildItemsList();
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock

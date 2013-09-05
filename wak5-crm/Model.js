546e6d55f2a5ac4dae923796a052327649b4ca77
David Robbins <dave@wakanda.org>
2013-09-05 14:26:03 -0700

    Model API defintion for User dataclass is now a module.

--- wak5-crm/Model.js
+++ wak5-crm/Model.js
@@ -1,10 +1,12 @@
 ﻿
 model = new DataStoreCatalog();
 
-include("classes/utility.js");
+include("classes/utility.js"); //Utility methods used my all dataclasses.
+
+//Use module pattern for User dataclass definition. I use this dataclass in many projects.
+model.User = require('User');
 
 include("classes/activity.js");
-include("classes/user.js");
 include("classes/lead.js");
 include("classes/contact.js");
 include("classes/account.js");
@@ -13,6 +15,7 @@ include("classes/note.js");
 include("classes/log.js");
 
 
+//Example for Wakanda Training on Model API (v5);
 var dataClassNames = ['Lead', 'Account', 'Contact']; 
 
 for (var i = 0, len = dataClassNames.length; i < len; i++) 
